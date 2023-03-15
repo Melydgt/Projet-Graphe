7e851dea70359c3c99f8bfc2ca17abbbbbc2ca5e
Melissa <melissa.deregnaucourt@efrei.net>
2023-03-15 12:38:15 +0100

    mise en memoire du graphe Gson 1

--- src/com/company/G2_Main.java
+++ src/com/company/G2_Main.java
@@ -1,9 +1,9 @@
 package com.company;
+
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+
+import com.google.gson.Gson;
 
 public class G2_Main {
 
@@ -21,13 +21,12 @@ public class G2_Main {
             System.out.println("Bonjour et bienvenue dans ce programme qui vous aides pour l'ordonnancement d'un graphe");
             System.out.println("Entrer le nom du fichier que vous voulez lire");
             name_file = sc.nextLine();
-            name_file = "./fichier_test/" + name_file + ".txt";
-            System.out.println("Nous ouvrons le fichier " + name_file);
+            System.out.println("Nous ouvrons le fichier " + name_file + ".txt");
 
             // Le fichier d'entrée par default
-//            File file = new File(name_file);
-            System.out.println("\n\n\tDefault table 1");
-            File file = new File("./fichier_test/table 12.txt");
+            File file = new File("./fichier_test/" + name_file + ".txt");
+//            System.out.println("\n\n\tDefault table 1");
+//            File file = new File("./fichier_test/table 1.txt");
 
             // Créer l'objet File Reader
             FileReader fr = new FileReader(file);
@@ -63,7 +62,7 @@ public class G2_Main {
                 i = new ArrayList<>();
             }
 
-            // Pour ajouter les contraintes de l'état final c-a-d les taches qui n'apparaissent pas dans les contraintes il faut boucler les contraintes
+            // BOUCLE Ajouter les predecesseurs de l'état final
             for (G2_Tache ta : graphe.getGraph_tach()) {
                 Iterator<G2_Tache> k = graphe.getGraph_tach().iterator();
                 boolean existe = false;
@@ -78,14 +77,65 @@ public class G2_Main {
             }
             graphe.appendGraph(new G2_Tache(tache_final+1,0,i));
 
+
             // AFFICHAGE
             System.out.println("Contenu du graphe : \n début=0\tfin=" + (tache_final+1));
             System.out.println(graphe);
             fr.close();
+
+            // SAVE sauvegarde du graphe à l'aide de GSON
+            String jsonStrGraphe = new Gson().toJson(graphe);
+            writeJsonToFile(jsonStrGraphe, name_file);
         }
         catch(IOException e)
         {
             e.printStackTrace();
         }
+    }
+
+    private static void writeJsonToFile(String jsonStr, String name_graphe) {
+        try {
+            String file = "./fichier_test/memoire.txt";
+            String list_file = "./fichier_test/liste_en_mem.txt";
+
+            String jsonStrFile = "";
+            String jsonStrList = "";
+
+            if (!fileEmpty(file)) { jsonStrFile = readJsonFromFile(file); }
+            if (!fileEmpty(list_file)) { jsonStrList = readJsonFromFile(list_file); }
+
+            PrintWriter jsonFile = new PrintWriter(file);
+            PrintWriter jsonList = new PrintWriter(list_file);
+
+            if (!jsonStrFile.equals("")) { jsonFile.print(jsonStrFile + "\n" + jsonStr); }
+            else{ jsonFile.print(jsonStr); }
+            if (!jsonStrList.equals("")) { jsonList.print(jsonStrList + "\n" + name_graphe); }
+            else { jsonList.print(name_graphe); }
+
+
+            jsonFile.close();
+            jsonList.close();
+            System.out.println("Saving successful");
+        } catch (IOException ioe) {
+            System.out.println("ERROR: " + ioe.getMessage());
+        }
+    }
+
+    private static String readJsonFromFile(String file) {
+        String str = "";
+        try {
+            Scanner jsonFile = new Scanner(new File(file));
+            str = jsonFile.nextLine();
+            jsonFile.close();
+        }
+        catch (IOException ioe) {
+            System.out.println("ERROR: " + ioe.getMessage());
+        }
+        return str;
+    }
+
+    private static boolean fileEmpty(String name_file) {
+        File file = new File(name_file);
+        return file.length() == 0;
     }
 }

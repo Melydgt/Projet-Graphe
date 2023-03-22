4854cd0905f26ad3652ab0e37979015e26d6cb95
Melissa <melissa.deregnaucourt@efrei.net>
2023-03-22 09:14:51 +0100

    Modif affichage

--- src/com/company/G2_Main.java
+++ src/com/company/G2_Main.java
@@ -34,6 +34,7 @@ public class G2_Main {
                 fr.close();
 
 // ------------ Accueil
+                System.out.println("\n------------------------------------------------------------------------");
                 System.out.println("\nBonjour et bienvenue dans ce programme pour l'ordonnancement d'un graphe\ttape 999 pour exit");
                 System.out.println("Graphe sauvegarder :");
                 System.out.println(sb);
@@ -64,8 +65,7 @@ public class G2_Main {
                         // SAVE sauvegarde du graphe à l'aide de GSON
                         String jsonStrGraphe = new Gson().toJson(graphe);
                         writeJsonToFile(jsonStrGraphe, name_file);
-                    }
-                    else {
+                    } else {
                         System.out.println("!!! Le fichier n'existe pas !!!");
                     }
                 }
@@ -73,35 +73,40 @@ public class G2_Main {
                 else {
                     if (!fileEmpty(list_file)) {
                         graphe = new Gson().fromJson(readONElineFromFile(choix, mem_file), G2_Graphe.class);
-                    }
-                    else {
+                    } else {
                         System.out.println("!!! Aucun graphe en mémoire !!!");
                     }
                 }
 
 
-// ------------ AFFICHAGE (Etape 2) a continuer ...
-                System.out.println("\t\tLoad Graphe");
+// ------------ AFFICHAGE (Etape 2)
+                System.out.println("\t\t** Load Graphe **");
                 System.out.println(graphe);
+                AffichageGraphe(graphe);
 
 // ------------ Matrice des valeurs (Etape 2)
                 AffichageMatrice(graphe);
 
 // ------------ Vérifier les propriétés (Etape 3)
-                System.out.printf("\nPoint d'entrée : %d\tPoint de sorties : %d\n", 0, graphe.getGraph_tach().size());
+                System.out.printf("\nPoint d'entrée : %d\tPoint de sorties : %d\n\n", 0, graphe.getGraph_tach().size());
                 System.out.println("Présence de circuit dans le graphe ?");
 
-                detectionCircuit(graphe);
-                System.out.println(graphe);
+//                detectionCircuit(graphe);
+//                System.out.println(graphe);
 
-                if (!arcNeg(graphe)) {
-                    System.out.println("Le circuit NE contient PAS d'arc négatif");
-                }
-                else {
-                    System.out.println("Le circuit contient un ou plusieurs arc(s) négatif");
-                }
+//                if (!arcNeg(graphe)) {
+//                    System.out.println("Le circuit NE contient PAS d'arc négatif");
+//                } else {
+//                    System.out.println("Le circuit contient un ou plusieurs arc(s) négatif");
+//                }
+
 
 // ------------ Calcule du rangs (Etape 4)
+                if (!detectionCircuit(graphe) && !arcNeg(graphe)) {
+                    System.out.println("Le circuit NE contient PAS d'arc négatif");
+                } else {
+                    System.out.println("Le circuit contient un ou plusieurs arc(s) négatif");
+                }
 
 // ------------ Date au plus tot (Etape 5)
 
@@ -197,7 +202,7 @@ public class G2_Main {
             PrintWriter jsonList = new PrintWriter(list_file);
 
             if (!jsonStrFile.equals("")) { jsonFile.print(jsonStrFile + jsonStr); }
-            else{ jsonFile.print(jsonStr); }
+            else { jsonFile.print(jsonStr); }
             if (!jsonStrList.equals("")) { jsonList.print(jsonStrList + name_graphe); }
             else { jsonList.print(name_graphe); }
 
@@ -247,8 +252,20 @@ public class G2_Main {
     }
 
 
+    private static void AffichageGraphe(G2_Graphe graphe) {
+        System.out.printf("\n* Affichage chemin du graphe\n%11s = %-3s\n", "Chemins  ", "Délais");
+        int countArc=0;
+        for (G2_Tache sommet : graphe.getGraph_tach()) {
+            for (int i = 0; i < sommet.getContrainte().size(); i++) {
+                System.out.printf("%3d --> %-3d = %-3d\n", sommet.getContrainte().get(i), sommet.getSommet(), sommet.getDelai());
+                countArc++;
+            }
+        }
+        System.out.printf("Il y a %d sommets et %d arcs\n", graphe.getGraph_tach().size(), countArc);
+    }
+
     private static void AffichageMatrice(G2_Graphe graphe) {
-        System.out.println("\nMatrice des valeur\n");
+        System.out.println("\n* Matrice des valeur\n");
 
         // Entête de la matrice
         System.out.printf("%4s%4d", "", 0);
@@ -284,7 +301,7 @@ public class G2_Main {
     }
 
 
-    private static void detectionCircuit(G2_Graphe graphe) {
+    private static boolean detectionCircuit(G2_Graphe graphe) {
 //        System.out.println("Find 0 in contrainte");
 //        G2_Graphe graphTemp = new G2_Graphe(graphe.getGraph_tach());
         for (G2_Tache ta_co : graphe.getGraph_tach()) {
@@ -294,32 +311,34 @@ public class G2_Main {
         }
 
         while (graphe.getGraph_tach().size() != 0 && !contrainteExiste(graphe)) {
-            System.out.println("Supprimer les entrées :");
-//            System.out.println("graphe.getGraph_tach().size() " + graphe.getGraph_tach().size());
+            System.out.println("* Detection de circuit\n* Méthode d'élimination des points d'entrée");
             for (int i=0; i<graphe.getGraph_tach().size(); i++) {
                 System.out.println("\t\t\tTEST Sommet = " + graphe.getGraph_tach().get(i).getSommet());
-//                System.out.println("Si - le sommet n'a pas de contrainte = " + graphe.getGraph_tach().get(i).getContrainte().isEmpty());
                 if (graphe.getGraph_tach().get(i).getContrainte().isEmpty()) {
+                    System.out.println("Point d'entrée : " + graphe.getGraph_tach().get(i).getSommet());
                     for (G2_Tache ta_co : graphe.getGraph_tach()) {
-    //                    if (graphe.getGraph_tach().get(i).getContrainte().isEmpty()) {
-//                            System.out.println("Si - le sommet est compris dans les contraintes = " + ta_co.getContrainte().contains(graphe.getGraph_tach().get(i).getSommet()) + ta_co.getContrainte());
                             if (ta_co.getContrainte().contains(graphe.getGraph_tach().get(i).getSommet())) {
-                                System.out.println("REMOVE - " + graphe.getGraph_tach().get(i).getSommet() + " FROM " + ta_co.getSommet());
+//                                System.out.println("REMOVE - " + graphe.getGraph_tach().get(i).getSommet() + " FROM " + ta_co.getSommet());
                                 ta_co.getContrainte().remove((Integer)graphe.getGraph_tach().get(i).getSommet());
                             }
     //                    }
                     }
-                    System.out.println("on supprime le sommet ..." + graphe.getGraph_tach().get(i));
+                    System.out.println("Suppression des points d'entrée");
                     graphe.getGraph_tach().remove(i);
                     i--;
+                    if (graphe.getGraph_tach().size() != 0) {
+                        System.out.println("Il reste des sommets ...");
+                    }
                 }
             }
         }
         if (contrainteExiste(graphe)) {
             System.out.println("Le graphe contient un circuit (boucle)");
+            return true;
         }
         else {
             System.out.println("Le graphe NE contient PAS de circuit");
+            return false;
         }
     }
 
